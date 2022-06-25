c9846d2ea9c125595cdfda7d7f99ad72092a020f
melaku05 <melakushiferaw05@gmail.com>
2022-06-25 20:43:47 +0300

    fix linter

--- student.rb
+++ student.rb
@@ -1,5 +1,5 @@
 class Student < Person
-  def initialize(classroom, age, name = 'unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     super(name, age, parent_permission)
   end
 
